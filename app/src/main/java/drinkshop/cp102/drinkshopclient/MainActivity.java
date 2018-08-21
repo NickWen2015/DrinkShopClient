6fb05034eb019deb00ada105c15588a3a54e8bf0
Nick Wen <y2ka.tw@gmail.com>
2018-08-21 15:26:22 +0800

    Nick 修改主畫面為 BottomNavigationView
    修改主畫面為 BottomNavigationView
    目前遇到的問題為下面icon跳出來文字才出來，位移也不太對，期望是圖跟文字一起。

--- app/src/main/java/drinkshop/cp102/drinkshopclient/MainActivity.java
+++ app/src/main/java/drinkshop/cp102/drinkshopclient/MainActivity.java
@@ -1,13 +1,73 @@
 package drinkshop.cp102.drinkshopclient;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.item_News://最新消息
+                    fragment = new NewsFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textNews);
+                    return true;
+                case R.id.item_Product://產品頁面
+                    fragment = new MapFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textProduct);
+                    return true;
+                case R.id.item_Member://會員中心
+                    fragment = new MapFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textMember);
+                    return true;
+                case R.id.item_Settings://設定
+                    fragment = new SettingsFragment();
+                    changeFragment(fragment);
+                    setTitle(R.string.textSettings);
+                    return true;
+                default:
+                    initContent();
+                    break;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        initContent();
+    }
+
+    private void initContent() {
+        Fragment fragment = new NewsFragment();
+        changeFragment(fragment);
+        setTitle(R.string.textNews);
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
     }
 }
