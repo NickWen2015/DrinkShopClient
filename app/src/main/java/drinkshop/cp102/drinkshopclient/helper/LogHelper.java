486ca452d23b1e58715a6b4b5191d933c4210aef
Mrosstro <ttosam01@gmail.com>
2018-09-06 18:26:06 +0800

    組長
    新增LogHelper 類別   Log小幫手（開關設定關閉即可關閉所有Log）

---
+++ app/src/main/java/drinkshop/cp102/drinkshopclient/helper/LogHelper.java
@@ -0,0 +1,27 @@
+package drinkshop.cp102.drinkshopclient.helper;
+
+import android.util.Log;
+
+
+/**
+ * LogHelper.e("MainActivity", "------------------");
+ * Log小幫手（開關設定關閉即可關閉所有Log）
+ *
+ **/
+public class LogHelper {
+    private static String tag;
+    private static String msg;
+    private static boolean condition = true;  //開關
+
+    public LogHelper() { }
+
+    public static void e(String tag, String msg) {
+        if (condition)
+            Log.e(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (condition)
+            Log.d(tag, msg);
+    }
+}
